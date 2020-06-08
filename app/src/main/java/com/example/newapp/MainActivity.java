cffe262983c1a109c47eed5abc435309f3f3a568
osulca <omarsulca@gmail.com>
2020-06-08 13:50:36 -0500

    ejemplo ciclo de vida e interaccion

---
+++ app/src/main/java/com/example/newapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.newapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("Ejemplo", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Ejemplo", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Ejemplo", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Ejemplo", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Ejemplo", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Ejemplo", "onDestroy");
+        Log.i("Ejemplo", "Haz salido de la app");
+    }
+
+    public void mostrarTexto(View view) {
+        TextView tv_mensaje = (TextView) findViewById(R.id.txt_mensaje);
+        tv_mensaje.setText("Hola a todos");
+    }
+}
